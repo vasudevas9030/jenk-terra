fa9c2b28c5ab01212180cb06d323b43e64bd2fc1
vasudevas9030 <161864552+vasudevas9030@users.noreply.github.com>
2024-08-12 15:08:37 +0530

    Update ansible.tf

--- ansible.tf
+++ ansible.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "nexus"{
     instance_type = var.instance_type_t3medium
     key_name = var.key_name
     security_groups = ["jenkins"]
-    user_data = file("nexus.sh")
+    user_data = file("./nexus.sh")
     tags={
         Name = "${var.ansible}-insatance"
     }
